1a57f4711275428c59777f526233e3eba06f2798
Your Name <you@example.com>
2022-12-09 00:18:22 +0300

    done with assigment

--- index.js
+++ index.js
@@ -21,3 +21,57 @@ const allWagesFor = function () {
     return payable
 }
 
+
+
+function  createGeneralManager(){
+    const Title = "GeneralManager";
+    console.log("GeneralManager");
+    }
+    
+    
+    function createPay(){
+    const Pay = "50,000 on 15 th May 2022";
+    console.log("50,000 on 15 th May 2022");
+    }
+    
+    
+    function createTimeInEvent(){
+    const TimeinEvents = "Beginning of work Day at 1.00pm";
+    console.log("Beginning of Work Day at 1.00pm to 6.00pm");
+    }
+    
+    
+    function createTimeOutEvent(){
+    const TimeOutEvents = "End of Day; at 6.00pm"
+    console.log("End of Working Day at 6.00pm")
+    }
+    
+    function DebtRecord(){
+    const DebtToPay  = "Debt of 20.000 to pay by 15 th April 2022"
+    console.log("Debt of 20,00 to pay before 15 th April 2022")
+    
+    }
+     function  createWorkHours(){
+    const HoursWorked = "Worked for 5hours"
+    console.log("5hoursworked")
+    
+     }
+    
+     function createWagesEarned(){
+    const WagesEarned = " Wages between 50,000k to 100,000k"
+    console.log("Wages between 50,000k and 100,000k ")
+     }
+    
+     function createAllWagesForGeoffery(){
+    const allWages = "Up to 100,000"
+    console.log("up to 100,000k")
+     }
+     function createFindingEmployeeByFirstName(){
+        const  FindingEmployeeByFirstName ="That is Geoffery";
+        console.log("Thast is Geoffery")
+  
+        }
+     function CreatePayRoll(){
+    const PayRoll = "100,000k"
+    console.log("100,00k")
+      }
